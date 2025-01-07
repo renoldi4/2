2ddde83213583700af5f9ce411311f951a677f20
renoldi4 <149823854+renoldi4@users.noreply.github.com>
2025-01-07 18:24:25 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,20 +1,22 @@
 <?php
-
 ini_set("soap.wsdl_cache_enabled", "0");
 ini_set("soap.wsdl_cache_ttl", "1");
 
-$tassi_di_conversione = json_decode(file_get_contents("tassi_conversione.json"), true);
+$tassi_di_conversione = json_decode(file_get_contents("valori.json"), true)["conversion_rates"];
 
 function convertCurrency($amount, $currency) {
     global $tassi_di_conversione;
 
+    if (!isset($tassi_di_conversione[$currency])) {
+        throw new SoapFault("Server", "Valuta non valida");
+    }
+
     $tasso = $tassi_di_conversione[$currency];
     $convertedAmount = number_format($amount * $tasso, 2);
-    return "Il valore di $amount EUR in $currency è: $convertedAmount";
+    return $convertedAmount;
 }
 
 $server = new SoapServer("test.wsdl");
 $server->addFunction("convertCurrency");
 $server->handle();
-
 ?>
